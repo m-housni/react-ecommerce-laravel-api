6866c8e899e4ebcf0ecddfe959a3777b215f218e
m-housni <mohamed.housni@um5s.net.ma>
2022-03-08 12:34:26 +0100

    commit

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -8,46 +8,52 @@ const Cart = () => {
     const cart = useSelector(state => state.cart)
 
     return (
-        <table className="ui sortable table">
-            <thead>
-                <tr>
-                    <th className="computer only">Product</th>
-                    <th>Price</th>
-                    <th>Qty</th>
-                    <th>Total</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    cart.products.map(product => {
-                        return (
-                            <tr className="top aligned" key={product.id}>
-                                <td className="computer only">
-                                    <img src={product.image} alt={product.title} style={{width:"100px"}} />
-                                </td>
-                                <td>${product.price}</td>
-                                <td className="top aligned">
-                                    <div className="ui mini input">
-                                        <input type="number" placeholder="Qty" value={product.qty} />    
-                                    </div>
-                                </td>
-                                <td>
-                                    ${product.qty * product.price}
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colSpan="3">
-                        <div style={{textAlign:"right"}}><span>Total</span></div>
-                    </td>
-                    <td>{cart.total}</td>
-                </tr>
-            </tfoot>
-        </table>
+        <div className="ui container">     
+            {
+                cart.products.map(product => {
+                    return (
+                        <div className="ui four column grid computer only" key={product.id}>
+                            <div className="column ">
+                                <img src={product.image} alt={product.title} style={{width:"100px"}} />
+                            </div>
+                            <div  className="column">
+                                {product.title}
+                            </div>
+                            <div  className="column">
+                                <div className="ui mini input">
+                                    <input type="number" value={product.qty} style={{width:"100%"}} />    
+                                </div>
+                            </div>
+                            <div  className="column">
+                                ${product.qty * product.price}
+                            </div>
+                        </div>
+                    )
+                })
+            }
+           {
+                cart.products.map(product => {
+                    return (
+                        <div className="ui three column grid mobile only" key={product.id}>
+                            <div className="column ">
+                                <img src={product.image} alt={product.title} style={{width:"32px"}} />
+                                <br />
+                                {product.title}
+                            </div>
+                            <div  className="column">
+                                <div className="ui mini input">
+                                    <input type="number" value={product.qty} style={{width:"100%"}} />    
+                                </div>
+                            </div>
+                            <div  className="column">
+                                ${product.qty * product.price}
+                            </div>
+                        </div>
+                    )
+                })
+                
+            }
+        </div>
     )
 }
 
